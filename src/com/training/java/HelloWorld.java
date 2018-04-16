8fbe2738194711a7c37b37c65d1c52ad2ae2a63b
Your Name <ramdafale@gmail.com>
2018-04-16 11:59:21 +0530

    First java program HelloWorld

---
+++ src/com/training/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.training.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// this code is printing Hello world
+System.out.println("Hello World");
+	}
+
+}
