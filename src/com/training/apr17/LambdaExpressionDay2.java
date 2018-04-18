6d88a265d2cac9136511a8b086c42583bc64d963
ramdafale <ramdafale@gmail.com>
2018-04-18 13:58:21 +0530

    Update LambdaExpressionDay2.java

--- src/com/training/apr17/LambdaExpressionDay2.java
+++ src/com/training/apr17/LambdaExpressionDay2.java
@@ -1,58 +1,118 @@
+
+/*******************************************************************************************************************
+ * Program Name: 		Lambda Expression 
+ * Program Description : Write a lambda expression for finding a palindrome of following
+ * 						1->IsOdd
+ * 						2->IsPrime
+ * 						3->IsPalindrome
+ * 
+ *Programmer Name: 		Ram Dafale
+ *Release Date:			17-April-2018
+ ********************************************************************************************************************/
+
 package com.tarining.apr17;
 
-public class LambdaExpressionDay2 {
+/*******************************************************************************************************************
+ * Class Name: LambdaExp Program Description : This class include the all
+ * Functional interface required. : Declaration of all Functional interfaces.
+ * 
+ * Programmer Name: Ram Dafale Release Date: 17-April-2018
+ ********************************************************************************************************************/
+
+public class LambdaExp {
 
 	// Functional Interface
-	interface CheckOdd {
+	interface IsOdd {
 		public boolean isOdd(int number);
 	}
 
 	// Functional Interface
-	interface CheckPrime {
+	interface IsPrime {
 		public boolean isPrime(int number);
 	}
 
 	// Functional Interface
-	interface CheckPalindrome {
-		public boolean isPalindrome(int number);
+	interface isPalindrome {
+		public boolean isPalindrome(int number, int flag);
 	}
 
-	public static void main(String[] args) {
+	/*******************************************************************************************************************
+	 * Method Name: LambdaExp Program Description : Main method is consists of 3
+	 * different logic for 3 activities of the program. 1. IsOdd method take 1
+	 * parameter and check weather it is odd or not. 2. IsPrime method take 1
+	 * parameter and check weather it is prime or not. 3. isPalindrome method take 1
+	 * parameter and check weather it is palindrome or not.
+	 ********************************************************************************************************************/
 
-		int no = 11;
+	public static void main(String[] args) {
+		// Taking input
+		int input = 121;
 
 		// lambda expression to check if a number is even/odd.
-		CheckOdd check1 = (number) -> {
-			if (no % 2 == 0)
+
+		/*******************************************************************************************************************
+		 * Method Name: IsOdd() Program Description : 1. IsOdd method take 1 parameter
+		 * and check weather it is odd or not.
+		 ********************************************************************************************************************/
+		IsOdd check1 = (number) -> {
+			if (input % 2 == 0)
 				return false;
 			else
 				return true;
 		};
-		System.out.println("No is odd : " + check1.isOdd(no));
+		System.out.println("No is odd : " + check1.isOdd(input));
 
-		// lambda expression to check if a number is prime/not.
-		CheckPrime prime1 = (number) -> {
-			for (int i = 2; i <= no / 2; i++) {
-				if (no % i == 0)
+		/*******************************************************************************************************************
+		 * Method Name: IsPrime() Program Description : 2. IsPrime() method take 1
+		 * parameter and check weather it is prime or not.
+		 ********************************************************************************************************************/
+
+		IsPrime prime1 = (number) -> {
+			for (int i = 2; i <= input / 2; i++) {
+				if (input % i == 0)
 					return false;
 			}
 			return true;
 		};
-		System.out.println("No is prime : " + prime1.isPrime(no));
+		System.out.println("No is prime : " + prime1.isPrime(input));
+
+		/*******************************************************************************************************************
+		 * Method Name: isPalindrome() Program Description : 3. isPalindrome() method
+		 * take 1 parameter and check weather it is palindrome or not.
+		 ********************************************************************************************************************/
 
 		// lambda expression to check if a number is palindrome.
-		int remainder = 0, reverse = 0;
-		CheckPalindrome palindrome1 = (number) -> {
-			while (no > 0) {
+
+		isPalindrome palindrome1 = (number, flag) -> {
+			int no1 = number;
+			int remainder = 0, sum = 0;
+			while (number > 0) {
 				remainder = number % 10;
 				number = number / 10;
-				reverse = reverse * 10 + remainder;
-				if (no == reverse)
-					return true;
-				else
-					return false;
+				flag = flag * 10 + remainder;
+				sum = flag;
 			}
-		};
+			System.out.println(sum);
+			if (sum == no1)
+				return true;
+			else
+				return false;
 
+		};
+		int flag = 0;
+		System.out.println("No is palindrome : " + palindrome1.isPalindrome(input, flag));
 	}
+
 }
+
+/********************************************
+ * Output: 
+ * No is odd : true
+No is prime : false
+121
+No is palindrome : true
+
+ */
+
+
+
