32dd2cd1878389461df94d3f36557bc32178f510
ramdafale <ramdafale@gmail.com>
2018-04-18 13:59:19 +0530

    Update ReorganizeString.java
    Reorganize String and shuffle

--- src/com/training/apr17/ReorganizeString.java
+++ src/com/training/apr17/ReorganizeString.java
@@ -1,3 +1,12 @@
+
+/*******************************************************************************************************************
+ * Program Name: 		 String Reorganizing
+ * Program Description : Take a word and make sure that no two letter occuur adjacent to one another.
+ * 
+ *Programmer Name: 		Ram Dafale
+ *Release Date:			17-April-2018
+ ********************************************************************************************************************/
+
 package com.tarining.apr17;
 
 import java.util.ArrayList;
@@ -6,28 +15,54 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
+/*******************************************************************************************************************
+ * Class Name: ReorganizeString Program Description :
+ * Description: It takes input from user and 
+ * 
+ ********************************************************************************************************************/
+
 public class ReorganizeString {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		String str, output;
-		int d, p, q;
-		List<Character> characters = new ArrayList<>();
+
+		// Taking input form user and storing in to the variable
+
 		System.out.print("Please enter the string: ");
-		str = input.nextLine();
-		for (int i = 0; i < str.length(); i++) {
-			characters.add(str.charAt(i));
+
+		Scanner sc = new Scanner(System.in);
+
+		String input = sc.nextLine();
+		sc.close();
+		int d;
+		List<Character> charactersToPrint = new ArrayList<>();
+
+		for (int i = 0; i < input.length(); i++) {
+			charactersToPrint.add(input.charAt(i));
 		}
-		Iterator itr = characters.iterator();
+
+		Iterator<Character> itr = charactersToPrint.iterator();
+
+		System.out.println("  \n Number To be Altered ");
+
 		while (itr.hasNext()) {
 			System.out.print(itr.next());
 		}
-		System.out.println();
-		d = str.length() - 2;
-		Collections.rotate(characters, -d);
 
-		System.out.print(characters);
+		// Logic to shuffle letters 
+		int rotateMe = input.length() - 2;
+		Collections.rotate(charactersToPrint, -rotateMe);
+
+		System.out.print("Im running successfully");
+		System.out.print(charactersToPrint);
 
 	}
+
+	/*
+	 * Output:
+	 * 
+	 * 
+	 * Please enter the string: aab aab [a, b, a]
+	 * 
+	 */
 
 }
