2ef5cc57d7341686e6c370be09ab9f4b40875f97
ramdafale <ramdafale@gmail.com>
2018-04-19 12:09:49 +0530

    palindrome PMD
    changes are written in comment inside program

--- src/com/training/palindromepmd/Palindrome.java
+++ src/com/training/palindromepmd/Palindrome.java
@@ -1,89 +1,114 @@
+
+// Commenting like java docs 
+
+/*Program Name: 		Lambda Expression 
+ * Program Description : Write a lambda expression for finding a
+ * 						 palindrome of following IsOdd,IsPrime,IsPalindrome
+ *Programmer Name: 		Ram Dafale
+ *Release Date:			17-April-2018
+*/
+
 package palindromepmd;
 
 /*
- *  Program name :		  Check if integer is palindrome or not.
- *  Program Description : It check if given number is read same from 
- *  					  left and right. 
- *   					  eg. 121, 131;
- * Programmer Name: Ram Dafale
- * DateOfRelease: 16-April-2018
+ * Class Name: LambdaExp Program Description : This class include the all
+ * Functional interface required. : Declaration of all Functional interfaces.
+ * 
+ * Programmer Name: Ram Dafale Release Date: 17-April-2018
  */
-
-
-
-//Library to take input from user in console
-import java.util.Scanner;
 import java.util.logging.Logger;
 
-// class
+public class LambdaExp {
 
-public class Palindrome    {
+	@FunctionalInterface
+	interface IsOdd {
+		public boolean isOdd(int number);
+	}
 
-	
+	@FunctionalInterface
+	interface IsPrime {
+		public boolean isPrime(int number);
+	}
+
+	@FunctionalInterface
+	interface isPalindrome {
+		public boolean isPalindrome(int number, int flag);
+	}
+
 	/*
-	 *Method Name: main
-	 *Description: It takes input from user. then it breaks the number
-	 *				 by getting remainder 
-	 *				then it adds that remainder to sum varibale. and then divide the number.
-	 *				this process repeats till we get same number as user specified.
-	 */	 
+	 * Method Name: LambdaExp Program Description : Main method is consists of 3
+	 * different logic for 3 activities of the program. 1. IsOdd method take 1
+	 * parameter and check weather it is odd or not. 2. IsPrime method take 1
+	 * parameter and check weather it is prime or not. 3. isPalindrome method take 1
+	 * parameter and check weather it is palindrome or not.
+	 */
 
-	public static void main(String args[]) {
-		final	  Scanner inputNumber = new Scanner(System.in); 
-	 final  	Logger printf = Logger.getLogger(Palindrome.class.getName());
-		 int num;
-		 int temp;
-		 int addition;
-		 addition= 0;
-		 int firstResult;
-		 // sum variable to 
-		 // try block to monitor exception and to avoid the wrong input from user
-		 
-		 
-	
-		 printf.info("Enter any number");
-	        num = inputNumber.nextInt();
-		 
-		
-		 // take input from user and store in a variable 
-	      
-	       
-	       // temporary variable to store user input 
-	        temp = num;
-	       // loop for checking the number greater than 0
-	        while(num <= 0)
-	        {
-	        	firstResult = num % 10; 
-	        	addition = addition * 10 + firstResult;
-	            num = num / 10;
-	        }
-	        // comparing given integer with calculated integer
-	        // if sum and temp are equal then given number is palindrome
-	        if(addition == temp)
-	        {
-	        	printf.info("Given number "+temp+" is Palindrome");
-	        }
-	        else
-	        {
-	        	printf.info("Given number "+temp+" is Not Palindrome");
-	        }
-	        inputNumber.close();
-		 }
+	// removed unnecessary String args from main
+	public static void main() {
 
-	
-		 
-		 // catch block to catch exception if any and show appropriate response to user
-		
-		
-		
-	      
+		final Logger printf = Logger.getLogger(LambdaExp.class.getName());
+
+		// Taking input
+		// declaring local variable final because it is not going to change
+
+		final int input = 121;
+
+		// lambda expression to check if a number is even/odd.
+
+		/*
+		 * Method Name: IsOdd() Program Description : 1. IsOdd method take 1 parameter
+		 * and check weather it is odd or not.
+		 */
+		IsOdd check1 = (number) -> {
+			if (input % 2 == 0)
+				return false;
+			else
+				return true;
+		};
+		printf.info("No is odd : " + check1.isOdd(input));
+
+		/*******************************************************************************************************************
+		 * Method Name: IsPrime() Program Description : 2. IsPrime() method take 1
+		 * parameter and check weather it is prime or not.
+		 ********************************************************************************************************************/
+
+		IsPrime prime1 = (number) -> {
+			for (int i = 2; i <= input / 2; i++) {
+				if (input % i == 0)
+					return false;
+			}
+			return true;
+		};
+		printf.info("No is prime : " + prime1.isPrime(input));
+
+		/*
+		 * Method Name: isPalindrome() Program Description : 3. isPalindrome() method
+		 * take 1 parameter and check weather it is palindrome or not.
+		 */
+
+		isPalindrome palindrome1 = (number, flag) -> {
+			// write a every new variable into new line
+			int no1 = number;
+			int remainder = 0;
+			int sum = 0;
+			while (number > 0) {
+				remainder = number % 10;
+				number = number / 10;
+				flag = flag * 10 + remainder;
+				sum = flag;
+			}
+			// Using Logger
+			printf.info(sum);
+			if (sum == no1)
+				return true;
+			else
+				return false;
+
+		};
+		int flag = 0;
+
+		printf.info("No is palindrome : " + palindrome1.isPalindrome(input, flag));
+
+	}
 
 }
-
-
-/*
-output:
-Enter any number:131
-Given number 131 is Palindrome
-*/
-
