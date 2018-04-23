711c28526c223fefd3feda077a53bb1cbb1878d2
ramdafale <ramdafale@gmail.com>
2018-04-23 19:35:40 +0530

    currentAccount all done
    latest commit 7.33pm

--- bank11/src/main/java/bankapplication/currentAccount.java
+++ bank11/src/main/java/bankapplication/currentAccount.java
@@ -4,27 +4,43 @@
 package bankapplication;
 
 /**
- * @author trainee
- *
+ * @author ram dafale This class create a current account and perform basic
+ *         deposit and witrdraw operations.
  */
+class currentAccount extends Account {
+	Customer customer;
+	double interestRate = 0.08;
 
-
-class currentAccount extends Account
-{
-	public currentAccount(double balance,Customer customer) {
-		super(balance,customer);
-		this.customer=customer;
+	public currentAccount(double balance, Customer customer) {
+		super(balance, customer);
+		this.customer = customer;
 		// TODO Auto-generated constructor stub
 	}
 
-    public void Withdraw(int amount)
-    {
-       
-        if (amount < 0)
-        {
-           
-        }
-        balance = balance - amount;
-        
-    }
+	/**
+	 * @param amount
+	 */
+	public double Deposit(int amount) {
+		return balance = balance + amount;
+	}
+
+	/**
+	 * @param amount
+	 */
+	public void Withdraw(int amount) {
+
+		if (amount < 0) {
+
+		}
+		balance = balance - amount;
+
+	}
+
+	public void addInterest() {
+
+		balance = interestRate * balance + balance;
+		System.out.println("Salary After adding interest rate is :" + balance);
+
+	}
+
 }
