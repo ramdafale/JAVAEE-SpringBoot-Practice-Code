7597ee029ba6810a0e9b26db4e70c2ade6a8d300
ramdafale <ramdafale@gmail.com>
2018-04-24 11:30:17 +0530

    Update statement.java

--- bank11/src/main/java/bankapplication/statement.java
+++ bank11/src/main/java/bankapplication/statement.java
@@ -1,7 +1,15 @@
+/*
+ * Program Name: Test cases for abnk application using JUNIT
+ * @author Ram Dafale
+ * DateOF Completion : 4/42/2018
+ */
+
+
+
 package BankApplicationWIthMaven.BankApplicationAI;
 
 
-import static org.junit.Assert.assertEquals;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
@@ -13,15 +21,24 @@ import bankapplication.Customer;
 
 
 
-
+/*
+ * class Name: BankAppJunit
+ * Description : this class contains various test cases for bank application 
+ * 				1. Null values are not allowed in Name
+ * 				2. Null values are not allowed in lASTnaME
+ * 				3. Null values are not allowed in MOBILE NUMBER
+ * 				4. checking if deposit method running or not
+ * 				5.	 checking if withdraw method running or not
+ * 				6. checking if addInterest method running or not
+ */			
 public class BankAppJunit  {
 
 	
-	
+	//Creating a customer class object
 	Customer cust = new Customer("ram", "dafale", "wardha", 97546545, "ramdafale@gmail.com");
 	
 
-	
+	// 1. Null values are not allowed in Name
 	@Test
 	public void checknullInFirstName()
 	{
