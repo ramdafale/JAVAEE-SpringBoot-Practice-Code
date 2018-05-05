4bd97391b7f75a2ab98f829bcbf8a99501f73430
ramdafale <ramdafale@gmail.com>
2018-05-06 02:31:22 +0530

    changes done

---
+++ Bank Application/src/main/java/bankapplication/Bank.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package bankapplication;
+
+/**
+ * @author trainee
+ *
+ */
+class Bank {
+	static String name = "-------Indian Bank------";
+
+	public void bankName() {
+
+		LOGGER.info(name);
+
+	}
+
+}
---
+++ Bank Application/src/main/java/bankapplication/Customer.java
@@ -0,0 +1,110 @@
+/**
+ * 
+ */
+package bankapplication;
+
+/**
+ * @author trainee Desc: class Customer has all required fields like firstname
+ *         ,lastname etc. this class is sending all field values to the Account
+ *         class
+ */
+
+class Customer {
+
+	private String fname = null;
+	private String lname = null;
+	private String address = null;
+	private long number;
+	private String email;
+
+	/**
+	 * @param fname
+	 * @param lname
+	 * @param address
+	 * @param number
+	 * @param email
+	 */
+	public Customer(String fname, String lname, String address, long number, String email) {
+		this.fname = fname;
+		this.lname = lname;
+		this.address = address;
+		this.number = number;
+		this.email = email;
+	}
+
+	/**
+	 * @return the fname
+	 */
+	public String getFname() {
+		return fname;
+	}
+
+	/**
+	 * @param fname
+	 *            the fname to set
+	 */
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	/**
+	 * @return the lname
+	 */
+	public String getLname() {
+		return lname;
+	}
+
+	/**
+	 * @param lname
+	 *            the lname to set
+	 */
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address
+	 *            the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the number
+	 */
+	public long getNumber() {
+		return number;
+	}
+
+	/**
+	 * @param number
+	 *            the number to set
+	 */
+	public void setNumber(long number) {
+		this.number = number;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
