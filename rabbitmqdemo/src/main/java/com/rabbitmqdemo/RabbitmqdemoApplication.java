9fcadfeec8d7e29fb71d5fca9098b6e01c8f67c1
IAm@WORK <ramdafale@gmail.com>
2018-06-06 20:59:32 +0530

    Add files via upload

---
+++ rabbitmqdemo/src/main/java/com/rabbitmqdemo/RabbitmqdemoApplication.java
@@ -0,0 +1,12 @@
+package com.rabbitmqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqdemoApplication.class, args);
+	}
+}
