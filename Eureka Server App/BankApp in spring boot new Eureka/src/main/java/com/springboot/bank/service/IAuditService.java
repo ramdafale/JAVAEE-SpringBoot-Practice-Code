2ad7230563a0d869903f91f0a4e76500723efd0c
Dafale <2033023@cognizant.com>
2025-07-16 14:15:25 +0530

    deleted extra file  :qw

--- Eureka Server App/BankApp in spring boot new Eureka/src/main/java/com/springboot/bank/service/IAuditService.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.bank.service;
-
-import com.springboot.bank.model.Audit;
-import com.springboot.bank.model.Customer;
-
-public interface IAuditService {
-
-	void auditLogService(Audit audit );
-}
